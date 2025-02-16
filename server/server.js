3bf91a6fc11461aa1790ddcff729bdc7ffaf712b
C.R. SANNUTH <136961566+CRSANNUTH@users.noreply.github.com>
2025-02-16 20:19:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use('/task', taskRouter);
 
 // localhost:4000/auth/register
 
-const port = 4000;
+const port = 8080;
 
 app.listen(port, () => {
 	console.log(`server is running on port`, port);
